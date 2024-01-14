65189c9c250d69bee72dd19db71a71ff11eef1df
Hristo Vakadinov <hristovakadinov@gmail.com>
2024-01-14 16:23:51 +0200

    Initial commit

--- src/BankBranch.java
+++ src/BankBranch.java
@@ -1,2 +1,22 @@
+import java.util.LinkedList;
+
 public class BankBranch {
+    private int branchId;
+    private String branchName;
+    private LinkedList<Customer> customers;
+
+    public BankBranch(int branchId, String branchName) {
+        this.branchId = branchId;
+        this.branchName = branchName;
+        this.customers = new LinkedList<Customer>();
+    }
+
+    public void addCustomer(Customer customer) {
+        this.customers.add(customer);
+    }
+
+    public LinkedList<Customer> getCustomers() {
+        return this.customers;
+    }
+
 }
